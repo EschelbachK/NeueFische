2b731e43f442904878c7eeebc6d8e53d928a88ec
Daniel Töpel <toepel@gmx.it>
2025-06-25 15:07:18 +0200

    Daniel upgedatet

--- Daniel.java
+++ Daniel.java
@@ -1,6 +1,8 @@
 public class Daniel {
 
     public static void main(String[] args) {
-        System.out.println("Test123");
+        for(int i = 0; i < 10; i++) {
+            if(i%3==0) System.out.println("Test" + i);
+        }
     }
 }
