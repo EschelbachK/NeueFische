ad8824ba00f0ff8c95238836fbeae5a0a4327504
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-06-25 15:23:37 +0200

    update3 EschelbachKai

--- TimMacrae.java
+++ TimMacrae.java
@@ -1,5 +1,5 @@
 public class TimMacrae {
     public static void main(String[] args) {
-        System.out.println("Update von tim");
+        System.out.println("Update von tim und Kai");
     }
 }
